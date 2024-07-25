9fbd419e02d9a5aa8611bd4599774168a3b62c17
joe-egan <joe.egan.23@ucl.ac.uk>
2024-07-25 17:03:57 +0100

    fix package structure

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     entry_points={
         "spey.backend.plugins": [
-            "contur.full_histogram_likelihood = src.likelihoods:ConturHistogram"
+            "contur.full_histogram_likelihood = src.contur_likelihood.likelihoods:ConturHistogram"
         ]
     }
 )
